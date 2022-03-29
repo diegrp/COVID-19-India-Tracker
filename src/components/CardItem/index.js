2a69b239921323551669d0c520a2ce00c7b35d25
Diego Rocha <dkxever@gmail.com>
2022-03-28 21:00:47 -0300

    Create initial structures of components required for the covid tracker app

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,13 @@
+import './CardItem.css';
+
+// Totalidade dos casos
+
+/* Componente que relata a totalidade de todos os casos */
+
+export const CardItem = () => {
+  return(
+    <div>
+      ...
+    </div>
+  )
+}
---
+++ src/components/TableItem/index.js
@@ -0,0 +1,13 @@
+import './TableItem.css';
+
+// Tabela de casos de infecção
+
+/* Detalhe de todos casos de infecção em todos os estados da Índia */
+
+export const TableItem = () => {
+  return(
+    <div>
+      ...
+    </div>
+  )
+}
