c994070c3f135cf175fb74347dbdad4d04d01028
Diego Rocha <dkxever@gmail.com>
2022-03-28 21:04:54 -0300

    Status and effect hooks with functions, for indiacase API, stateSearch and formateDate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
 const App = () => {
+
+  const [ totalIndiaCase, setTotalIndiaCase ] = useState([]);
+  const [ loading, setLoading ] = useState(false);
+  const [ totalStateWiseCount, setTotalStateWiseCount ] = useState([]);
+  const [ totalStateArrayLength, setTotalStateArrayLength ] = useState('');
+  let [ filteredData ] = useState();
+
+  useEffect(() => {
+    loadData();
+  },[]);
+
+  // Carrega os dados da API
+
+  /* acessa a api e pega os dados manipulados e sua respectiva quantidade */
+
+  const loadData = async () => {
+    setLoading(true);
+    const resp = await axios.get("https://data.covid19india.org/data.json");
+    setTotalIndiaCase(resp.data.statewise.slice(0, 1));
+    const totalStateCount = resp.data.statewise.slice(1);
+    setTotalStateWiseCount(totalStateCount);
+    setTotalStateArrayLength(totalStateCount.length);
+    setLoading(false);
+  };
+
+  // Procura o estado
+
+  /* tenta procurar estados que corresponde com o que esteja procurando */
+
+  const stateSearch = (searchText) => {
+    filteredData = totalStateWiseCount.filter((value) => {
+      return value.state.toLowerCase().includes(searchText.toLowerCase());
+    });
+    setTotalStateWiseCount(filteredData);
+  };
+
   return(
     <main>
 
